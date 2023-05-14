4f7008e80125c0804dbd16316f3603522ae7c3b1
HongChaa <126743377+HongChaa@users.noreply.github.com>
2023-05-15 03:01:34 +0900

    ClubBoardListResponseDTO 내동호회조회

---
+++ src/main/java/com/hrms/project4th/mvc/dto/ClubBoardListResponseDTO.java
@@ -0,0 +1,11 @@
+package com.hrms.project4th.mvc.dto;
+
+import lombok.*;
+
+@Setter @Getter
+@ToString @EqualsAndHashCode
+@NoArgsConstructor @AllArgsConstructor
+@Builder
+public class ClubBoardListResponseDTO {
+    private String empName;
+}
